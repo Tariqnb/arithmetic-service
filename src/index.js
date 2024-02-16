9352c55361583be3bdf27b93178ef23fbc2a5d97
Tareq <tnabi@iu.edu>
2024-02-15 19:26:49 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const {add} = require("./arithmetica");
 const app = express();
 app.use(cors());
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World!');
 });
+app.get('/add/:n/:m',(req,res)=>{
+    let n = Number (req.params.n);
+    let m = Number (req.params.m);
+    let sum = add(n,m);
+    res.json(sum);
+});
+
 app.get('/add/:n/:m', (req, res) => {
     res.json({ calculationResult: Number(req.params.n) + Number(req.params.m) });
 });
